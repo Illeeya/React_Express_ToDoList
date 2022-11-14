af576ddad57fa4ba9a6dfc5694a29f7319ee0e10
Illeeya <domkoziak@gmail.com>
2022-11-14 20:29:39 +0100

    Backend upload || Glitch version

--- node_react_tester_001/backend/myBackend.js
+++ node_react_tester_001/backend/myBackend.js
@@ -1,191 +1,144 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
-const fs = require("fs");
-const path = require("path");
+// CODE IS MEANT TO BE USED ON GLITCH.COM
+// EXPRESS
 const express = require("express");
 const cors = require("cors");
-const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
 const server = express();
-
-// const tasks = require("./data/tasks.json");
-// const test = require("./data/student-2.json");
-const db = new sqlite3.Database("./data/test.db", (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Connected to the in-memory SQlite database.");
-  console.log(db.tables);
-});
 
 server.use(cors());
 server.use(express.json());
 
-// let rawdata = fs.readFileSync('./data/tasks.json');
-// let student = JSON.parse(rawdata);
-// console.log(student);
-
-// console.log(tasks);
-
-db.serialize(function () {
-  db.each("SELECT a, taskText FROM Athlete", function (err, row) {
-    console.log("User: ", row);
-  });
+// SQLITE3
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("tasksData", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the test SQlite database:");
 });
 
-db.close();
-
-// let student = {
-//   name: "Mike",
-//   age: 23,
-//   gender: "Male",
-//   department: "English",
-//   car: "Honda",
-// };
-
-// let data = JSON.stringify(student);
-// fs.writeFileSync("./data/student-2.json", data);
-
-// console.log(data);
-
+// APP START
 let todoState = "";
+let errorStatus = "";
 
-server
-  .route("/")
-  .post((req, res) => {
-    todoState = JSON.stringify(req.body);
-    // fs.writeFile("./data/tasks.json", todoState);
-    console.log(`Save request for data string: `, JSON.stringify(req.body));
-    res.send(`Data has been saved`);
-  })
-  .get((req, res) => {
-    //console.log("Get request came");
-    // let a = JSON.parse(todoState);
-    // a.tasks = a.tasks.map((task) => {
-    //   return {
-    //     id: task.id,
-    //     content: `${task.content}Doned`,
-    //   };
-    // });
-    // console.log(a);
-    if (todoState !== "") res.status(200).send(todoState);
-    else res.status(500).send("Nothing to sync");
-    //res.status(200).send("Worked");
-  });
+function saveTasksToDb(userId) {
+  console.log("Called save");
+  try {
+    db.run(`delete from Tasks where ownerId = ${userId}`);
 
-server.listen(5000);
+    todoState.tasks.forEach((task) => {
+      db.run(
+        `insert into Tasks values('${task.key}', '${task.content}', ${userId})`
+      );
+    });
+  } catch (e) {
+    errorStatus = e;
+  }
+}
 
-// Require the fastify framework and instantiate it
-// const fastify = require("fastify")({
-//   // Set this to true for detailed logging:
-//   logger: false,
-// });
+// ROUTING
 
-// // ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+server.post("/tasksSave", (req, res) => {
+  todoState = req.body;
+  console.log(`Save request for data string: `, JSON.stringify(req.body));
+  saveTasksToDb(req.body.userId);
+  if (errorStatus === "") res.send(`Data has been saved`);
+  else res.send(`Data might not have been saved. Error: ${errorStatus}`);
+});
 
-// // Setup our static files
-// fastify.register(require("@fastify/static"), {
-//   root: path.join(__dirname, "public"),
-//   prefix: "/", // optional: default '/'
-// });
+server.post("/tasksLoad", (req, res) => {
+  console.log("Post on tasks came");
+  const user = req.body.userId;
+  console.log(user);
 
-// // Formbody lets us parse incoming forms
-// fastify.register(require("@fastify/formbody"));
+  const sql = `SELECT taskKey, taskText FROM Tasks WHERE ownerId = ${user} ORDER BY taskKey`;
 
-// // View is a templating manager for fastify
-// fastify.register(require("@fastify/view"), {
-//   engine: {
-//     handlebars: require("handlebars"),
-//   },
-// });
+  try {
+    db.all(sql, (err, rows) => {
+      if (rows) {
+        let tasks = [];
+        rows.forEach((row) => {
+          let id = row.taskKey;
+          let content = row.taskText;
+          let taskObject = { id: id, content: content };
+          console.log(taskObject);
+          tasks.push(taskObject);
+        });
 
-// // Load and parse SEO data
-// const seo = require("./src/seo.json");
-// if (seo.url === "glitch-default") {
-//   seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-// }
+        if (tasks === []) res.status(500).send("No tasks to load");
+        else res.status(200).send(JSON.stringify({ tasks: tasks }));
+      } else if (err) {
+        console.log(err);
+        res.status(500).send(`No tasks found, error: `);
+      } else {
+        console.log("No tasks for user");
+        res.status(500).send("No tasks found for that user.");
+      }
+    });
+  } catch (error) {
+    console.log("Catch error: ", error);
+    res.status(500).send(`Db error: ${error}`);
+  }
+});
 
-// /**
-//  * Our home page route
-//  *
-//  * Returns src/pages/index.hbs with data built into it
-//  */
-// fastify.get("/", function (request, reply) {
-//   // params is an object we'll pass to our handlebars template
-//   let params = { seo: seo };
+server.post("/users/login", async (req, res) => {
+  console.log("Post on users came");
 
-//   // If someone clicked the option for a random color it'll be passed in the querystring
-//   if (request.query.randomize) {
-//     // We need to load our color data file, pick one at random, and add it to the params
-//     const colors = require("./src/colors.json");
-//     const allColors = Object.keys(colors);
-//     let currentColor = allColors[(allColors.length * Math.random()) << 0];
+  const user = req.body.username;
+  const pass = req.body.password;
 
-//     // Add the color properties to the params object
-//     params = {
-//       color: colors[currentColor],
-//       colorError: null,
-//       seo: seo,
-//     };
-//   }
+  const sql = `SELECT userId, password FROM Users WHERE username = UPPER('${user}');`;
 
-//   // The Handlebars code will be able to access the parameter values and build them into the page
-//   return reply.view("/src/pages/index.hbs", params);
-// });
+  try {
+    db.get(sql, async (err, row) => {
+      if (err) console.log("Inner err: ", err);
+      if (row) {
+        console.log("Row: ", row.userId);
+        if (await bcrypt.compare(req.body.password, row.password)) {
+          res.status(200).send(`${row.userId}`);
+        } else {
+          res.status(400).send("Wrong password!");
+        }
+      } else {
+        res.status(400).send(`Wrong username!`);
+      }
+    });
+  } catch (error) {
+    console.log("Outter err: ", error);
+  }
+});
 
-// /**
-//  * Our POST route to handle and react to form submissions
-//  *
-//  * Accepts body data indicating the user choice
-//  */
-// fastify.post("/", function (request, reply) {
-//   // Build the params object to pass to the template
-//   let params = { seo: seo };
+server.post("/users/register", async (req, res) => {
+  console.log("Registering user");
 
-//   // If the user submitted a color through the form it'll be passed here in the request body
-//   let color = request.body.color;
+  const user = req.body.username;
+  const pass = req.body.password;
 
-//   // If it's not empty, let's try to find the color
-//   if (color) {
-//     // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
+  console.log(user, pass);
 
-//     // Load our color data file
-//     const colors = require("./src/colors.json");
+  if (user.length <= 0 || pass.length <= 0) {
+    res.status(500).send("Username or password are empty. Cannot create user.");
+  } else {
+    try {
+      const hashedPassword = await bcrypt.hash(pass, 10);
+      console.log("Hashed pass: ", hashedPassword);
+      const sql = `INSERT INTO Users(username, password) VALUES('${user.toUpperCase()}', '${hashedPassword}')`;
+      console.log(sql);
+      db.exec(sql, (err) => {
+        if (err) {
+          res
+            .status(500)
+            .send(`Could not add user to database. Error: ${err.message}`);
+        } else {
+          res.status(200).send("User created!");
+        }
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .send(`Could not register the user. Error: ${error.message}`);
+    }
+  }
+});
 
-//     // Take our form submission, remove whitespace, and convert to lowercase
-//     color = color.toLowerCase().replace(/\s/g, "");
-
-//     // Now we see if that color is a key in our colors object
-//     if (colors[color]) {
-//       // Found one!
-//       params = {
-//         color: colors[color],
-//         colorError: null,
-//         seo: seo,
-//       };
-//     } else {
-//       // No luck! Return the user value as the error property
-//       params = {
-//         colorError: request.body.color,
-//         seo: seo,
-//       };
-//     }
-//   }
-
-//   // The Handlebars template will use the parameter values to update the page with the chosen color
-//   return reply.view("/src/pages/index.hbs", params);
-// });
-
-// // Run the server and report out to the logs
-// fastify.listen(
-//   { port: process.env.PORT, host: "127.0.0.1" },
-//   function (err, address) {
-//     if (err) {
-//       fastify.log.error(err);
-//       process.exit(1);
-//     }
-//     console.log(`Your app is listening on ${address}`);
-//     fastify.log.info(`server listening on ${address}`);
-//   }
-// );
+server.listen(3000);
